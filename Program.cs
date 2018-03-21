86f2bad01a0385e8bc36edad61ac75f027dab1e3
Matheusmmr7 <37145611+Matheusmmr7@users.noreply.github.com>
2018-03-21 09:12:02 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            Console.WriteLine("codigo 1: Calcular 10% do numero");
+            Console.WriteLine("codigo 2: calcular a % de um numero");
+            Console.WriteLine("codigo 3: identificar se é par ou impar");
+            Console.WriteLine("codigo 4: Calcular a raiz de uma função de 1°");
+            Console.WriteLine("codigo 5: Calcular a raiz de uma função de 2°");
+            Console.WriteLine("Digite o codigo: ");
+            int Cod = Convert.ToInt32(Console.ReadLine());
+
+        
+        }
+    }
+}
